87ae521170e3f355c99c2627fb6ef540b8c8098a
Alexandr <venomskiy@yandex.ru>
2024-12-25 02:14:27 +0400

    Сверстал макет, создал основную структуру, добавил скрипт и по умолчанию подключил необходимые элементы к нему.

---
+++ app.js
@@ -0,0 +1,13 @@
+const inputFirst = document.querySelector('#input-first');
+const inputSecond = document.querySelector('#input-second');
+const buttonPlus = document.querySelector('#button-plus');
+const buttonMinus = document.querySelector('#button-minus');
+const answerField = document.querySelector('.response__number');
+
+buttonPlus.onclick = () => {
+    console.log('plus click')
+}
+
+buttonMinus.onclick = () => {
+    console.log('minus click')
+}
